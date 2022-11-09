05462a8e0086a82f4afadec3da0e97f0418fe762
Antonio Marcos <ambrcz@hotmail.com>
2022-11-09 02:18:36 -0300

    feat: Adicionando a função e editar e remover tarefas

--- index.js
+++ index.js
@@ -1,83 +1,130 @@
-// Colocando a data de hoje na página inicial e no dia
-const dataPre = new Date();
-const date = new Intl.DateTimeFormat("pt-br").format(dataPre);
-const areaDataTarefas = document.querySelector("#hoje-de-tarefas");
-areaDataTarefas.innerHTML = `${date}`;
-const areaAnoDias = document.querySelector("#ano-de-dias");
-areaAnoDias.innerHTML = `${date.slice(-4)}`;
-const areaDataDias = document.querySelector("#data-de-dias");
-areaDataDias.innerHTML = `${date.slice(0, 5)}`;
-//Janelas
-const windowAddTask = document.querySelector(".tela-de-adicionar");
-const windowEditTask = document.querySelector(".tela-de-editar");
-const windowRemoveTask = document.querySelector(".tela-de-excluir");
+//Datas
+const preDate = new Date();
+const localeDate = new Date().toLocaleString().slice(0, 10);
+const day = localeDate.slice(0, 2);
+const month = localeDate.slice(3, 5);
+const year = localeDate.slice(6, 10);
 
-//Botões
-const buttonAddDays = document.querySelector("#adicionar-de-dias");
-const buttonAddTask = document.querySelector("#adicionar-de-tarefas");
-const buttonWindowAdd_Add = document.querySelector("#adicionar-de-adicionar");
-const buttonWindowEdit_Cancel = document.querySelector("#cancelar-de-editar");
-const buttonWindowEdit_Save = document.querySelector("#salvar-de-editar");
-const buttonWindowRemove_Cancel = document.querySelector("#cancelar-de-excluir");
-const buttonWindowRemove_Remove = document.querySelector("#excluir-de-excluir");
+document.querySelector("#data-de-dias").innerHTML = `${day}/${month}`;
+document.querySelector("#ano-de-dias").innerHTML = year;
+document.querySelector("#hoje-de-tarefas").innerHTML = localeDate;
+
+let listaTarefas = [];
+
+//Janelas
+const windowAdd = document.querySelector("#tela-de-adicionar");
+const windowEdit = document.querySelector("#tela-de-editar");
+const windowRemove = document.querySelector("#tela-de-excluir");
+
+//Formulários
+const formAdd = document.querySelector("#form-de-adicionar");
 
 //Areas
 const areaTasks = document.querySelector(".tarefas");
+const areaRemoveActivity = document.querySelector("#atividade-de-excluir");
+const areaEditActivity = document.querySelector("#atividade-de-editar");
+
+//Botões
+const addInDaysBtn = document.querySelector("#adicionar-de-dias");
+const addInTaskBtn = document.querySelector("#adicionar-de-tarefas");
+
+const btnWindowRemoveCancel = document.querySelector("#cancelar-de-excluir");
+const btnWindowRemoveRemove = document.querySelector("#excluir-de-excluir");
+const btnWindowEditCancel = document.querySelector("#cancelar-de-editar");
+const btnWindowEditSave = document.querySelector("#salvar-de-salvar");
 
 //Inputs
-const inputName = document.querySelector("#nome-da-tarefa");
-const inputTask = document.querySelector("#tarefa");
-const inputDate = document.querySelector("#data");
+const inputAddName = document.querySelector("#nome-da-tarefa");
+const inputAddTask = document.querySelector("#tarefa");
+const inputAddDate = document.querySelector("#data");
+const inputEditName = document.querySelector("#nome-da-tarefa-editar");
+const inputEditTask = document.querySelector("#tarefa-editar");
+const inputEditDate = document.querySelector("#data-editar");
 
-//Fazendo os botões abrirem as paginas correspondentes
-buttonAddDays.addEventListener("click", () => {
-    windowAddTask.style.display = "flex";
-    inputName.value = "";
-    inputTask.value = "";
-    inputDate.value = "";
-})
+// Funções
+function janelaRemover(event) {
+    let index = event.target.getAttribute('ordem-tarefa');
+    areaRemoveActivity.innerHTML = "<strong>" + listaTarefas[index].nome + "</strong>";
+    windowRemove.style.display = "flex";
+    btnWindowRemoveCancel.addEventListener("click", () => {
+        windowRemove.style.display = "none";
+    });
+    btnWindowRemoveRemove.addEventListener("click", removerListaTarefas)
+}
 
-buttonAddTask.addEventListener("click", () => {
-    windowAddTask.style.display = "flex";
-    inputName.value = "";
-    inputTask.value = "";
-    inputDate.valueAsDate = new Date(Date.UTC(dataPre.getFullYear(), dataPre.getMonth(), dataPre.getDate()));
-})
+function janelaEditar(event) {
+    let index = event.target.getAttribute('ordem-tarefa');
+    areaEditActivity.innerHTML = "<strong>" + listaTarefas[index].nome + "</strong>";
+    inputEditName.value = listaTarefas[index].nome;
+    inputEditTask.value = listaTarefas[index].detalhes;
+    windowEdit.style.display = "flex";
+    btnWindowEditCancel.addEventListener("click", () => {
+        windowEdit.style.display = "none";
+    });
+}
 
-buttonWindowAdd_Add.addEventListener("click", () => {
-    if (inputDate.value.split("-").reverse().join("/") === date) {
-        if (inputTask.value != "" && inputName.value != "") {
-            const title = document.createElement("h1");
-            title.innerText = inputName.value;
-            const description = document.createElement("p");
-            description.innerText = inputTask.value;
-            const task = document.createElement("div");
-            task.classList.add("task");
-            const buttonEdit = document.createElement("button");
-            buttonEdit.id = 'editor';
-            const buttonRemove = document.createElement("button");
-            buttonRemove.id = 'removedor';
-            task.appendChild(title);
-            task.appendChild(description);
-            task.appendChild(buttonEdit);
-            task.appendChild(buttonRemove);
-            areaTasks.appendChild(task);
-            buttonRemove.addEventListener("click", () => {
-                windowRemoveTask.style.display = "flex";
-                document.querySelector("#atividade").innerText = inputName.value;
-                buttonWindowRemove_Cancel.addEventListener("click", () => {
-                    windowRemoveTask.style.display = "none";
-                })
-                buttonWindowRemove_Remove.addEventListener("click", () => {
-                    areaTasks.remove(task);
-                    windowRemoveTask.style.display = "none";
-                })
-            })
+function removerListaTarefas(event) {
+    let index = event.target.getAttribute('ordem-tarefa');
+    listaTarefas.splice(index, 1);
+    atualizarListaTarefas();
+    windowRemove.style.display = "none";
+}
 
-            buttonEdit.addEventListener("click", () => {
-                windowEditTask.style.display = "flex"
-            })
-        }
+function atualizarListaTarefas() {
+    areaTasks.innerHTML = "";
+    for (let i = 0; i < listaTarefas.length; i++) {
+        const tarefa = listaTarefas[i];
+        const task = document.createElement("div");
+        task.classList.add("task");
+        const title = document.createElement("h1");
+        title.innerText = tarefa.nome;
+        const description = document.createElement("p");
+        description.innerText = tarefa.detalhes;
+        const buttonEdit = document.createElement("button");
+        buttonEdit.classList.add("editor");
+        buttonEdit.setAttribute('ordem-tarefa', i);
+        buttonEdit.addEventListener("click", janelaEditar);
+        const buttonRemove = document.createElement("button");
+        buttonRemove.classList.add("removedor");
+        buttonRemove.setAttribute('ordem-tarefa', i);
+        buttonRemove.addEventListener("click", janelaRemover);
+        task.appendChild(title);
+        task.appendChild(description);
+        task.appendChild(buttonEdit);
+        task.appendChild(buttonRemove);
+        areaTasks.appendChild(task);
     }
-    windowAddTask.style.display = "none";
-})
+}
+
+//Adicionando funcionalidades
+windowAdd.addEventListener("click", (event) => {
+    if (!formAdd.contains(event.target)) {
+        windowAdd.style.display = "none";
+    }
+});
+
+addInDaysBtn.addEventListener("click", () => {
+    windowAdd.style.display = "flex";
+    inputAddName.value = "";
+    inputAddTask.value = "";
+    inputAddDate.value = "";
+});
+
+addInTaskBtn.addEventListener("click", () => {
+    windowAdd.style.display = "flex";
+    inputAddName.value = "";
+    inputAddTask.value = "";
+    inputAddDate.valueAsDate = new Date(Date.UTC(preDate.getFullYear(), preDate.getMonth(), preDate.getDate()));
+});
+
+formAdd.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (inputAddDate.value.split("-").reverse().join("/") === localeDate) {
+        listaTarefas.push({
+            nome: inputAddName.value,
+            detalhes: inputAddTask.value,
+        })
+        atualizarListaTarefas();
+    }
+    windowAdd.style.display = "none";
+});
